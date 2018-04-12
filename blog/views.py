515f553f6aa5ff0be038b2af78cba63da98b9e7f
chris129 <chrisli129@gmail.com>
2018-04-12 23:31:39 +0800

    引入markdown和语法高亮显示

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@
 
 from django.shortcuts import render,get_object_or_404
 from .models import Post
+import  markdown
 
 def index(request):
     #注意此post_list后面千万不能参数，不然类型就从QuerySet变成了tunple
@@ -18,4 +19,17 @@ def detail(request,pk):
     就返回对应的 post，如果不存在，就给用户返回一个 404 错误，表明用户请求的文章不存在。
     """
     post = get_object_or_404(Post,pk=pk)
-    return render(request,'blog/detail.html',context={'post':post})
+    """
+    这样我们在模板中展示 {{ post.body }} 的时候，就不再是原始的 Markdown 文本了，而是渲染过后的 HTML 文本。注意这里我们给 markdown 
+    渲染函数传递了额外的参数 extensions，它是对 Markdown 语法的拓展，这里我们使用了三个拓展，分别是 extra、codehilite、toc。extra 
+    本身包含很多拓展，而 codehilite 是语法高亮拓展，这为我们后面的实现代码高亮功能提供基础，而 toc 则允许我们自动生成目录
+    文章详情页没有看到预期的效果，而是类似于一堆乱码一样的 HTML 标签，这是由于安全考虑， HTML 代码在 Django 的模板中都会被转义，
+    在模板中找到展示博客文章主体的 {{ post.body }} 部分，为其加上 safe 过滤器，{{ post.body|safe }}，大功告成，这下看到预期效果了
+    """
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                     'markdown.extensions.extra',
+                                     'markdown.extensions.codehilite',
+                                     'markdown.extensions.toc',
+                                  ])
+    return render(request, 'blog/detail.html', context={'post': post})
