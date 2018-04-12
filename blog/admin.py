6808b7811b55f99294cd26e079224a9d3f1d2977
chrisli129 <chrisli129@gmail.com>
2018-04-12 17:43:45 +0800

    注册到admin后台进行文章发布和管理

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Post,Category,Tag
-##注册服务到Django后台管理
+##注册服务到Django后台管理，使用后台发布文章
 admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(Tag)
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from .models import Post
 
 def index(request):
+    #注意此post_list后面千万不能参数，不然类型就从QuerySet变成了tunple
     post_list = Post.objects.all().order_by('-created_time')
     return render(request,'blog/index.html',context={'post_list':post_list}
     )
