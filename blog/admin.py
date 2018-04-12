db6c8c38dad8e6edfb826f98a05ae0b7df7775ab
chrisli129 <chrisli129@gmail.com>
2018-04-12 17:40:22 +0800

    花费2个小时的views错误

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Post,Category,Tag
+##注册服务到Django后台管理
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Tag)
